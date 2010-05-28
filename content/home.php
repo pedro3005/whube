0dc74f71f03d6b05743f49d969a4feb6ad22ecc3
Paul Tagliamonte <paultag@ubuntu.com>
2010-05-27 23:04:35 -0400

    asfaf

--- content/home.php
+++ content/home.php
@@ -18,6 +18,7 @@ EOF;
 
 $CONTENT .= "<a href = '" . $SITE_PREFIX . "t/logout' >Logout</a><br />";
 $CONTENT .= "<a href = '" . $SITE_PREFIX . "t/new-bug' >New Bug</a><br />";
+$CONTENT .= "<a href = '" . $SITE_PREFIX . "t/bug-list' >List latest bugs</a><br />";
 
 
 
