92c412772b4c5e2445c2618a8786b1d4652f6eff
Paul Tagliamonte <paultag@ubuntu.com>
2010-06-01 00:11:59 -0400

    some flavor

--- index.php
+++ index.php
@@ -8,6 +8,25 @@ $CONTENT = "
 Whube v 3.141
 <br />
 <a href = '" . $SITE_PREFIX . "t/login' >Go Home!</a>
+<br /><br />
+<div class = 'center' >
+<table>
+
+	<tr>
+		<td><img src = '" . $SITE_PREFIX . "imgs/users.png' alt = '' /></td>
+		<td><img src = '" . $SITE_PREFIX . "imgs/bugs.png' alt = '' /></td>
+		<td><img src = '" . $SITE_PREFIX . "imgs/design.png' alt = '' /></td>
+		<td><img src = '" . $SITE_PREFIX . "imgs/new-user.png' alt = '' /></td>
+	</tr>
+	<tr>
+		<td>Login</td>
+		<td>Search for some Bugs</td>
+		<td>Find a project</td>
+		<td>Create an account</td>
+	</tr>
+
+</table>
+</div>
 ";
 
 include( "view/view.php" );
