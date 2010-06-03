145e0340df596854593cb29237b9ab6f5b8d8b2c
Paul Tagliamonte <paultag@ubuntu.com>
2010-06-03 12:26:01 -0400

    small updates <3

--- index.php
+++ index.php
@@ -1,33 +1,10 @@
 <?php
 session_start();
 include ( "conf/site.php" );
+include ( "libs/php/core.php" );
 
 $TITLE   = "Welcome!";
-$CONTENT = "
-<h1>Welcome to Whube!</h1>
-Whube v 3.141
-<br />
-<a href = '" . $SITE_PREFIX . "t/login' >Go Home!</a>
-<br /><br />
-<div class = 'center' >
-<!-- <table>
-
-	<tr>
-		<td><img src = '" . $SITE_PREFIX . "imgs/users.png' alt = '' /></td>
-		<td><img src = '" . $SITE_PREFIX . "imgs/bugs.png' alt = '' /></td>
-		<td><img src = '" . $SITE_PREFIX . "imgs/design.png' alt = '' /></td>
-		<td><img src = '" . $SITE_PREFIX . "imgs/new-user.png' alt = '' /></td>
-	</tr>
-	<tr>
-		<td>Login</td>
-		<td>Search for some Bugs</td>
-		<td>Find a project</td>
-		<td>Create an account</td>
-	</tr>
-
-</table> -->
-</div>
-";
+$CONTENT = $ABOUT_WHUBE;
 
 include( "view/view.php" );
 
---
+++ libs/php/core.php
@@ -0,0 +1,42 @@
+<?php
+
+$VERSION         = "3.141";
+$VERSION_STRING  = "v$VERSION";
+
+$WHUBE_PROJECT_LEAD   =  "Paul Tagliamonte";
+$WHUBE_PROJECT_URL    =  "http://whube.com/";
+
+$ABOUT_WHUBE = <<<EOF
+
+<h1>Namaste, and Welcome. This is Whube</h1>
+
+<b>Whube</b> is a hip bug tracker. Not hip in the way
+that old people use it, but hip as in I think multitouch
+and acceleromters are hip. It's a bit rough around the 
+edges and is really not stable enough for corprate use.
+Not to mention I don't think it will scale well. But that
+is neither here nor there. <br />
+<br />
+There is lots of AJAX and Javascript. To be sure, let me test
+to ensure there is no dumb stuff going on.<br />
+<div class = 'shit' id = 'remove-me' >
+	<div class = 'content' >
+		Dude! TURN ON JAVASCRIPT! Don't be such a lamer!
+	</div>
+</div>
+<script type = 'text/javascript' >
+	$('#remove-me').hide();
+</script>
+<br />
+Do you see the big red scary text above? No? w00t. That's a good thing.
+<br />
+<br />
+<br />
+Peace, Love and PHP,<br />
+EOF;
+
+$ABOUT_WHUBE .= "&nbsp;&nbsp;" . $WHUBE_PROJECT_LEAD;
+$ABOUT_WHUBE .= "<br />&nbsp;&nbsp;Project Lead, Whube";
+
+
+?>
