615a76aa5927394b76befe7ad346916946d6ee05
Paul Tagliamonte <paultag@ubuntu.com>
2010-06-03 10:07:00 -0400

    doing a small var move to keep site clean

--- conf/sql.php
+++ conf/sql.php
@@ -3,4 +3,6 @@
 	$mysql_user = "root";
 	$mysql_pass = "password";
 	$mysql_data = "whube";
+	$CREATE_DB     =   1; // If database exists, change to 0.
+	$TABLE_PREFIX  =   "whube_"; // So that those on shared hosts with one db can still use it
 ?>
