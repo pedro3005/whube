89a2954138a45b4768a98f48dcccfbf6890e1202
Paul Tagliamonte <paultag@ubuntu.com>
2010-06-03 00:13:30 -0400

    one last shot

--- view/foot.php
+++ view/foot.php
@@ -3,7 +3,6 @@
 		<div class = "foot" >
 			Copyright (c) Whube Hackers, 2009 - <?php echo date("o"); ?><br />Peace and Love to Y'all.
 				<div class = "clear" ></div>
-			</div>
 		</div>
 	</body>
 </html>
