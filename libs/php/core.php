e2fc38b3d1a856a99deec0d8af0dd38d0eb613c0
Paul Tagliamonte <paultag@ubuntu.com>
2010-06-04 01:51:22 -0400

    die. this looks bad

--- libs/php/core.php
+++ libs/php/core.php
@@ -35,8 +35,8 @@ Do you see the big red scary text above? No? w00t. That's a good thing.
 Peace, Love and PHP,<br />
 EOF;
 
-$ABOUT_WHUBE .= "&nbsp;&nbsp;" . $WHUBE_PROJECT_LEAD;
-$ABOUT_WHUBE .= "<br />&nbsp;&nbsp;Project Lead, Whube";
+// $ABOUT_WHUBE .= "&nbsp;&nbsp;" . $WHUBE_PROJECT_LEAD;
+// $ABOUT_WHUBE .= "<br />&nbsp;&nbsp;Project Lead, Whube";
 
 
 ?>
