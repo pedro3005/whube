4aacb19c850db08cee95c085ce39c63baa9639b0
Paul Tagliamonte <paultag@ubuntu.com>
2010-06-04 00:19:47 -0400

    asfasf

--- content/home.php
+++ content/home.php
@@ -1,26 +1,12 @@
 <?php
 
 include( "model/user.php" );
+include( "libs/php/core.php" );
 
 requireLogin();
 
 $TITLE    = "Welcome Home!";
-
-$realname = $_SESSION['real_name'];
-
-$CONTENT .= <<<EOF
-<h1>Welcome Home!</h1>
-So, welcome back, $realname. Looks like you are
-logged in and everything. Boohyeah. Well this, as
-you know, is Whube.<br />
-<br />
-EOF;
-
-/*
-$CONTENT .= "<a href = '" . $SITE_PREFIX . "t/logout' >Logout</a><br />";
-$CONTENT .= "<a href = '" . $SITE_PREFIX . "t/new-bug' >New Bug</a><br />";
-$CONTENT .= "<a href = '" . $SITE_PREFIX . "t/bug-list' >List latest bugs</a><br />";
-*/
+$CONTENT = $ABOUT_WHUBE;
 
 
 
