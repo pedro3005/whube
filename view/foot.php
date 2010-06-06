bfe93d1a77d48b2acb0d700c1b8680150a1dca0a
tenach <tenach@gmail.com>
2010-06-05 21:17:15 -0800

    fogots some files

--- view/foot.php
+++ view/foot.php
@@ -4,5 +4,20 @@
 			Copyright (c) Whube Hackers, 2009 - <?php echo date("o"); ?><br />Peace and Love to Y'all.
 				<div class = "clear" ></div>
 		</div>
+    <?php
+    if ( isset ( $PIWIK ) && $PIWIK ) {
+      $view_root = dirname(  __FILE__ ) . "/";
+      include($view_root . "../libs/php/piwik.php");
+      $piwik = piwikJS();
+    ?>
+    <script type="text/javascript">
+    <?php
+      print $piwik["script"];
+    ?>
+    </script>
+    <noscript><p><img src="http://stats.tenach.net/piwik.php?idsite=<?php echo $piwik["siteID"]; ?>" style="border:0" alt="" /></p></noscript>
+    <?php
+    }
+    ?>
 	</body>
 </html>
