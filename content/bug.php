95a143bed62e835ed58cae5917da042a22073dd8
Paul Tagliamonte <paultag@ubuntu.com>
2010-06-18 21:23:43 -0400

    revised bug interface

--- content/bug.php
+++ content/bug.php
@@ -168,27 +168,90 @@ $CONTENT .= "</select></td>
 
 }
 
+
 $CONTENT .= "
-This bug is against <b>" . $project['project_name'] . "</b><br />
-<b>" . $reporter['real_name'] . " ( " . $reporter['username'] . " )</b>, that troublemaker, reported this bug.<br />
+<table>
+
+	<tr>
+		<td>Project</td>
+		<td></td>
+		<td>" . $project['project_name'] . " ( <a href = '" . $SITE_PREFIX . "t/project/" . $project['project_name'] . "' >" . $project['project_name'] . "</a> )</td>
+	</tr>
+
+	<tr>
+		<td>Reporter</td>
+		<td></td>
+		<td>" . $reporter['real_name'] . " ( <a href = '" . $SITE_PREFIX . "t/user/" . $reporter['username'] . "' >" . $reporter['username'] . "</a> )</td>
+	</tr>
+	<tr>
+		<td>Created</td>
+		<td></td>
+		<td>" . date( "F j, o", $row['startstamp'] ) . "</td>
+	</tr>
+	<tr>
+		<td>Last Modified</td>
+		<td></td>
+		<td>" . date( "F j, o", $row['trampstamp'] ) . "</td>
+	</tr>
+
+	<tr>
+		<td>Owner</td>
+		<td></td>
 ";
 
-$CONTENT .= " It was created <b>" . date( "F j, o", $row['startstamp'] ) . "</b>. ";
-$CONTENT .= " It was last updated <b>" . date( "F j, o", $row['trampstamp'] ) . "</b>.<br /> ";
-
 	if ( isset ( $owner['uID'] ) ) {
-		$CONTENT .= "This bug is being hacked on by <b>" . $owner['real_name'] . "</b><br />";
+		$CONTENT .= "
+		<td>" . $owner['real_name'] . " ( <a href = '" . $SITE_PREFIX . "t/user/" . $owner['username'] . "' >" . $owner['username'] . "</a> )</td>";
 	} else {
-		$CONTENT .= "Sadly, this bug has no owner. Adopt me!</b><br />";
+		$CONTENT .= "
+		<td>No Owner</td>";
 	}
 
-	$status   = getStatus(   $row['bug_status'] );
-	$severity = getSeverity( $row['bug_severity'] );
 
-	$CONTENT .= "It's classified as a <b>" . $status['status_name'] . "</b> bug with a severity level of ";
-	$CONTENT .= "<b>" . $severity['severity_name'] . "</b>\n";
-	$CONTENT .= "<h1>Bug Description</h1>\n";
-	$CONTENT .= "<br />\n<pre>" . $row['descr'] . "</pre>\n";
+
+$CONTENT .= "
+
+	</tr>
+";
+
+$status   = getStatus(   $row['bug_status'] );
+$severity = getSeverity( $row['bug_severity'] );
+
+$statusIcon   = "";
+$severityIcon = "";
+
+if ( $status['critical'] ) {
+$statusIcon   = "<img src = '" . $SITE_PREFIX . "imgs/warning.png' alt = 'Critical' />";
+} else {
+$statusIcon   = "<img src = '" . $SITE_PREFIX . "imgs/happy.png' alt = 'Non-Critical' />";
+}
+
+if ( $severity['critical'] ) {
+$severityIcon   = "<img src = '" . $SITE_PREFIX . "imgs/warning.png' alt = 'Critical' />";
+} else {
+$severityIcon   = "<img src = '" . $SITE_PREFIX . "imgs/happy.png' alt = 'Non-Critical' />";
+}
+
+
+$CONTENT .= "
+
+	<tr>
+		<td>Status</td>
+		<td>" . $statusIcon . "</td>
+		<td>" . $status['status_name'] . "</td>
+	</tr>
+	<tr>
+		<td>Severity</td>
+		<td>" . $severityIcon . "</td>
+		<td>" . $severity['severity_name'] . "</td>
+	</tr>
+
+
+</table>
+<br />
+<h1>Bug Description</h1>
+<pre>" . $row['descr'] . "</pre>
+";
 
 } else {
 	$_SESSION['err'] = "Bug #" . $argv[1] . " does not exist!";
