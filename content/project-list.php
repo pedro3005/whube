27f8ea42574903700ace4af3bae6f8fc83e2bd96
Paul Tagliamonte <paultag@ubuntu.com>
2010-06-29 09:20:41 -0400

    starting work on project list

--- content/project-list.php
+++ content/project-list.php
@@ -11,4 +11,30 @@ include( "model/bug.php" );
 include( "model/user.php" );
 include( "model/project.php" );
 
+$TITLE = "Latest $Count projects";
+
+$i = 0;
+
+$CONTENT .= "<h1>Last $Count projects created</h1>";
+
+$CONTENT .= "
+<table>
+	<tr>
+		<th>Name</th> <th>Owner</th> <th>Bugs</th> <th>Private</th>
+	</tr>
+";
+
+$p = new project();
+$p->getAll();
+
+while ( $row = $p->getNext() ) {
+
+
+	$i++;
+}
+
+$CONTENT .= "
+</table><br /><br />
+";
+
 ?>
