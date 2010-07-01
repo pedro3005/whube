8d76f7c1925d13c8315db7a6250c0390520b8b6a
Paul Tagliamonte <paultag@ubuntu.com>
2010-06-30 21:32:55 -0400

    small tweeked up stuff

--- localcallback.php
+++ localcallback.php
@@ -96,25 +96,7 @@
 				$d['message'] = "Email ( " . $meta['email'] . " ) Unknown";
 			}
 /*
-			$m = new mailer();
-			$m->setTo(       $meta['email'] );
-			$m->setSubject(  "Bug Commands Processed" );
-
-$body = "Following are the results from the last whube bug email\n\n";
-
-if ( $d['errors'] ) {
-	$body .= "There were errors. The response was: " . $d['message'] . "\n";
-} else {
-	$body .= "No errors. Response: " . $d['message'] . "\n";
-}
-
-$body .= "\nThanks for your bug report!\n\n";
-$body .= " -- Whube Bugs\n\n";
-
-			$m->setBody(
-				$body
-			);
-			$m->send();
+    CREATE A NEW EVENT TYPE THING HERE
 */
 		} else {
 			$d['errors']  = true;
