d8771557872c38b4fa4a118f266bad5fbb78cada
Paul Tagliamonte <paultag@ubuntu.com>
2010-07-01 20:47:03 -0400

    attention whore changes

--- view/head.php
+++ view/head.php
@@ -68,6 +68,7 @@ if ( isset ( $_SESSION['id'] ) && $_SESSION['id'] > 0) {
 ?>
 	<a href = "<?php echo $SITE_PREFIX; ?>t/new-bug" >New Bug</a> | 
 	<a href = "<?php echo $SITE_PREFIX; ?>t/logout" >Logout</a>
+    ( You're <span class = 'itsme' ><?php echo $_SESSION['real_name']; ?></span>, Right? )
 <?php
 } else {
 ?>
