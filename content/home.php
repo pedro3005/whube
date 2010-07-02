b76c885e36f38e6544432f0437754824d1fd4d48
Paul Tagliamonte <paultag@ubuntu.com>
2010-07-01 20:38:04 -0400

    making whube nicer to the user

--- content/home.php
+++ content/home.php
@@ -6,8 +6,9 @@ include( "libs/php/core.php" );
 requireLogin();
 
 $TITLE    = "Welcome Home!";
-$CONTENT = $ABOUT_WHUBE;
 
+$CONTENT  = "<br /><h1>Heyya, " . $_SESSION['real_name'] . "</h1>Welcome Home.<br /><br /><br />\n";
+$CONTENT .= $ABOUT_WHUBE;
 
 
 ?>
